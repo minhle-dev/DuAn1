3951543724be0cf1bba4e6497e20b702cbf1a2e0
linkyo4 <160897+linkyo4@users.noreply.github.com>
2019-12-11 21:40:47 +0700

    aa
    aa

--- MilkTeaApplication/app/src/main/java/com/example/milkteaapplication/Common/Common.java
+++
@@ -1,9 +0,0 @@
-package com.example.milkteaapplication.Common;
-
-
-import com.example.milkteaapplication.Model.User;
-
-public class Common {
-    public static final String USER_REFERENCES = "User";
-    public static User currentUser;
-}
